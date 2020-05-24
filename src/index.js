fbff5e021101b43e7f2514bdfa57b31b5a9169dc
ralmeidax <ricardo.de.almeidax@gmail.com>
2020-05-24 11:35:50 -0300

    Reset password

--- src/index.js
+++ src/index.js
@@ -9,8 +9,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-require('./controllers/authController')(app)
-require('./controllers/projectController')(app)
+require('./app/controllers/index')(app);
+
+// require('./app/controllers/authController')(app)
+// require('./app/controllers/projectController')(app)
 
 // app.get('/', (req, res) => {
 //     res.send('OK')
